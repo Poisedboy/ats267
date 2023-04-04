155b8b34b3d7390876d2e9ffef9e8e437a789b78
Serhii <beschasnuy96@gmail.com>
2023-04-04 14:04:19 +0300

    projects, media pages are added

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,11 +3,12 @@ import ArtCenter from '../components/ArtCenter';
 import HumanCenter from '../components/HumanCenter';
 import Shelter from '../components/Shelter';
 import Team from '../components/Team';
-import Projects from '../components/Projects';
 import Blog from '../components/Blog';
 import Supporting from '../components/Supporting';
 import AboutUs from '../components/AboutUs';
 import Contacts from "../components/Contacts";
+import Media from "../components/Media";
+import Projects from "../components/Projects";
 
 export default function Root() {
     return (
@@ -17,8 +18,9 @@ export default function Root() {
         <Route path='humanitarian-center' element={<HumanCenter />}>Humanitarian Center</Route>
         <Route path='shelter' element={<Shelter />}>Shelter For Refugees</Route>
         <Route path='team' element={<Team />}>Our Team</Route>
-        <Route path='projects' element={<Projects />}>Our Projects</Route>
+        <Route path='media' element={<Media />}>Media</Route>
         <Route path='blog' element={<Blog />}>Blog</Route>
+        <Route path='projects' element={<Projects />}>Projects</Route>
         <Route path='support' element={<Supporting />}>Support Us</Route>
         <Route path='contacts' element={<Contacts />}>Contacts</Route>
       </Routes>
